9073ede3ac25219c433432a65321e19dad96272d
user <user@192.168.1.6>
2021-07-19 23:31:50 +0530

    first commit

---
+++ working_session/src/com/code/package1/addition.java
@@ -0,0 +1,9 @@
+package com.code.package1;
+
+public class addition {
+
+
+protected int add(int a , int b) {
+	return a+b;
+}
+}
---
+++ working_session/src/com/code/package1/test.java
@@ -0,0 +1,15 @@
+package com.code.package1;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		int a= 2;
+		int b=3;
+		addition add = new addition();
+		int c= add.add(a, b);
+		System.out.println("Addition of " + a + " and " + b + "=" + c );
+	}
+
+}
